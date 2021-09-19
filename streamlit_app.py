69759c9917a7ecc4f9b6cade25edacbf421edf2b
hauselin <hauselin@gmail.com>
2021-09-19 15:57:41 -0600

    Fix range issues

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,7 +55,6 @@ cols = list(keys.keys())
 colnames = list(keys.values())
 
 lucky = st.sidebar.button(f"I'm feeling lucky", key="lucky")
-# lucky = False
 
 if lucky:
     cols2 = cols[:28]
@@ -73,8 +72,12 @@ else:
     except:
         default_x = cols.index("years_postdoc")
         default_y = cols.index("n_offers_research")
+        default_x = cols.index("n_interview_teaching_final")
+        default_y = cols.index("n_offers_research")
+        st.session_state["default_y"] = default_y
+        st.session_state["default_x"] = default_x
 
-swap = st.sidebar.checkbox(f"Swap xy-axis variables", key="swap", value=False)
+swap = st.sidebar.button(f"Swap xy-axis variables", key="swap")
 if swap:
     default_y, default_x = default_x, default_y
 
@@ -83,38 +86,58 @@ st.sidebar.markdown("#### ")
 # %% sidebar
 
 xcol = st.sidebar.selectbox("x-axis variable", colnames, index=default_x)
-xcol = list(keys.keys())[list(keys.values()).index(xcol)]
+xcol_idx = list(keys.values()).index(xcol)
+xcol = list(keys.keys())[xcol_idx]
+st.session_state["default_x"] = xcol_idx
 remove_outliers_x = st.sidebar.checkbox(f"Exclude outliers", value=False, key="xcol")
 if remove_outliers_x:
     df[xcol] = utils.na_outliers(df[xcol])
 if xcol == "funding":
     df[xcol] = np.log10(df[xcol] + 1)
 
+xmin = int(np.floor(df[xcol].min()))
+xmax = int(df[xcol].max() + 1)
+if (xmax - xmin) <= 1:
+    st.warning("Insufficient range.")
+    st.stop()
 xmin, xmax = st.sidebar.select_slider(
     f"Range",
-    options=range(int(np.floor(df[xcol].min())), int(df[xcol].max() + 1)),
+    options=range(xmin, xmax),
     value=(int(df[xcol].min()), int(df[xcol].max())),
     key="xcol_slider",
 )
 df = df.loc[df[xcol] <= xmax].loc[df[xcol] >= xmin].reset_index(drop=True)
+if df.shape[0] <= 5:
+    st.warning("Insufficient data points.")
+    st.stop()
 
 st.sidebar.markdown("#### ")
 
 ycol = st.sidebar.selectbox("y-axis variable", colnames, index=default_y)
-ycol = list(keys.keys())[list(keys.values()).index(ycol)]
+ycol_idx = list(keys.values()).index(ycol)
+ycol = list(keys.keys())[ycol_idx]
+st.session_state["default_y"] = ycol_idx
 remove_outliers_y = st.sidebar.checkbox(f"Exclude outliers", value=False, key="ycol")
 if remove_outliers_y:
     df[ycol] = utils.na_outliers(df[ycol])
 if ycol == "funding":
     df[ycol] = np.log10(df[ycol] + 1)
 
+ymin = int(np.floor(df[ycol].min()))
+ymax = int(df[ycol].max() + 1)
+if (ymax - ymin) <= 1:
+    st.warning("Insufficient range.")
+    st.stop()
 ymin, ymax = st.sidebar.select_slider(
     f"Range",
-    options=range(int(np.floor(df[ycol].min())), int(df[ycol].max() + 1)),
+    options=range(ymin, ymax),
     value=(int(df[ycol].min()), int(df[ycol].max())),
     key="ycol_slider",
 )
 df = df.loc[df[ycol] <= ymax].loc[df[ycol] >= ymin].reset_index(drop=True)
+if df.shape[0] <= 5:
+    st.warning("Insufficient data points.")
+    st.stop()
 
 if xcol == ycol:
     st.warning("Select different x and y variables.")
@@ -125,10 +148,11 @@ st.sidebar.markdown("#### ")
 dfclean = df[[ycol, xcol]].dropna()
 n = dfclean.shape[0]
 
-degree = np.random.randint(1, 7, 1)[0] if lucky else 1
+degree = np.random.randint(1, 9, 1)[0] if lucky else 1
 order = st.sidebar.slider(
     "Regression polynomial order", min_value=1, max_value=15, value=int(degree)
 )
+
 
 # %% regress and correlate
 
@@ -137,7 +161,7 @@ degrees = [order]
 poly = PolynomialFeatures(degree=order)
 pipeline = make_pipeline(poly, LinearRegression())
 pipeline.fit(dfclean[[xcol]], dfclean[ycol])
-scores.append(pipeline.score(dfclean[[xcol]], df[ycol]))
+scores.append(pipeline.score(dfclean[[xcol]], dfclean[ycol]))
 
 cor = pg.corr(df[xcol], df[ycol])
 if isinstance(cor["BF10"][0], str):
@@ -147,23 +171,26 @@ if cor["p-val"][0] < 0.001:
 
 # %% plot
 
-brush = alt.selection_interval()
-
 df = df[[ycol, xcol, "gender"]].dropna()
 if ycol == "funding":
     df[ycol] = 10 ** df[ycol] - 1
 if xcol == "funding":
     df[xcol] = 10 ** df[xcol] - 1
 
-rge = [df[xcol].min(), df[xcol].max()]
+rge_x = [df[xcol].min(), df[xcol].max()]
+rge_y = [df[ycol].min(), df[ycol].max()]
 tick_axis = alt.Axis(labels=False, domain=False, ticks=False)
+
+# this type conversion is needed to plot regression line
+df[xcol] = df[xcol].astype("float")
+df[ycol] = df[ycol].astype("float")
 
 base = (
     alt.Chart(df)
     .mark_circle(size=55)
     .encode(
-        alt.X(xcol, scale=alt.Scale(domain=rge), title=keys[xcol]),
-        alt.Y(ycol, title=keys[ycol]),
+        alt.X(xcol, scale=alt.Scale(domain=rge_x), title=keys[xcol]),
+        alt.Y(ycol, title=keys[ycol], scale=alt.Scale(domain=rge_y)),
         tooltip=[xcol, ycol],
         color=alt.Color(
             "gender:N", title=keys["gender"], scale=alt.Scale(scheme="inferno")
@@ -179,38 +206,33 @@ x_density = (
         density="value",
         bandwidth=0.3,
         groupby=["gender"],
-        extent=rge,
+        extent=rge_x,
         counts=True,
         steps=34,
     )
     .mark_area(opacity=0.8)
     .encode(
-        alt.X("value:Q", title="", scale=alt.Scale(domain=rge), axis=tick_axis),
+        alt.X("value:Q", title="", scale=alt.Scale(domain=rge_x), axis=tick_axis),
         alt.Y("density:Q", stack="zero", axis=tick_axis, title=""),
-        alt.Color("gender:N"),
         tooltip=["gender", alt.X("value:Q")],
+        color=alt.Color(
+            "gender:N", title=keys["gender"], scale=alt.Scale(scheme="inferno")
+        ),
     )
     .properties(width=400, height=55)
 )
 
-polynomial_fit = [
-    base.transform_regression(
-        xcol, ycol, method="poly", order=order, as_=[xcol, str(order)]
-    )
-    .mark_line()
-    .transform_fold([str(order)], as_=["degree", ycol])
-    .encode()
-]
-
 reg = (
     alt.Chart(df)
-    .mark_point()
     .encode(x=xcol, y=ycol)
     .transform_regression(xcol, ycol, method="poly", order=order)
-    .mark_line(color="#5ec962", size=2)
+    .mark_line(
+        color="#5ec962",
+        size=2,
+    )
 )
 
-fig1 = reg + base
+fig1 = base + reg
 fig1 = fig1.properties(height=377).interactive()
 fig2 = fig1 & x_density
 fig2 = (
